6f8641c2529ae50c3f965bd7ee524daaf6aed0f5
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-21 04:29:42 +0000

    fix: ContactDetails.jsx, data visualization

--- src/js/views/ContactDetails.jsx
+++ src/js/views/ContactDetails.jsx
@@ -8,20 +8,17 @@ export const ContactDetails = () => {
   const { store, actions } = useContext(Context);
   // recuperar el parámetro de la url
   const params = useParams();
-  console.log(params)
-  console.log(store.users[params.contactId])
   const subindice = params.contactId
   const person = store.users[subindice]
-  console.log(person)
 
   return (
       <div className="card" style={{ width: "18rem" }}>
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{person ? person.name: ''}</h5>
-          <p className="card-text">{person ? person.email: ''}</p>
-          <p className="card-text">{person ? person.company.name: ''}</p>
-          <p className="card-text">{person ? person.address.city: ''}</p>
+          <h5 className="card-title">{() => person.name}</h5>
+          <p className="card-text">{() => person.email}</p>
+          <p className="card-text">{() => person.company.name}</p>
+          <p className="card-text">{() => person.address.city}</p>
         </div>
         <Link to={`/contact`} className="btn btn-success">Contact</Link>
       </div>
