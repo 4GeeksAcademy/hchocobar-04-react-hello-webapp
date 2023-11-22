9c3200adc8a0f8eb620d324eb238e4543623a37f
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-22 16:50:29 +0000

    flux.js: adding syntax comment

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,15 +2,17 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			demo: [{title: "FIRST", background: "white", initial: "white"},
- 						 {title: "SECOND", background: "white", initial: "white"}]
+ 						 {title: "SECOND", background: "white", initial: "white"},
+						 {title: "SECOND", background: "white", initial: "white"}]
 		},
 		actions: {
 			exampleFunction: () => {getActions().changeColor(0, "green");},  // Use getActions to call a function within a fuction
 			loadSomeData: () => { /*fetch().then().then(data => setStore({ "foo": data.bar }))*/ },
 			changeColor: (index, color) => {
-				const store = getStore();  // Get the store
+				/* const store = getStore();  // Get the store
 				// We have to loop the entire demo array to look for the respective index and change its color
-				const demo = store.demo.map((element, i) => {
+				const demo = store.demo.map((element, i) => { */
+				const demo = getStore.demo.map((element, i) => { 
 					if (i === index) {
 						element.background = color
 					};
@@ -23,3 +25,22 @@ const getState = ({ getStore, getActions, setStore }) => {
 };
 
 export default getState;
+
+
+/* 
+Syntax into actions:
+
+1. Usar getActions() para llamar a otra actions(function) dentro de una actions(fuction)
+	getActions().changeColor(0, "green")
+
+2. Utilizar getStore() para acceder al valor de un "store" dentro de un "actions"
+		2.1
+			const store = getStore();
+			store.demo.map()
+		2.2
+			getStore().demo.map()
+
+3. Utilizar setStore() para guardar un valor en un "store" (recordar que store es un objeto)
+	setStore({ demo: demo });
+
+*/
