496dfe23b0d95224a2f7275cceb14586259e4eaa
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-27 15:58:41 +0000

    fix: navbar style

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -9,40 +9,40 @@ export const Navbar = () => {
 
 
 	return (
-		<nav class="navbar navbar-expand-lg navbar-light bg-light bg-body-tertiary">
-			<div class="container-fluid d-flex justify-content-between mx-md-4 mt-3 mb-1">
+		<nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+			<div className="container-fluid d-flex justify-content-between mx-md-4 mt-3 mb-1">
 				<div>
 					<Link to="/" className="navbar-brand mb-0">
 						<span>{store.cohorte}</span>
 					</Link>
 				</div>
 				<div>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
 					</button>
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							<li class="nav-item">
-								<a class="nav-link active" aria-current="page" href="#">Home</a>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#">Home</a>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Link</a>
+							<li className="nav-item">
+								<a className="nav-link" href="#">Link</a>
 							</li>
-							<li class="nav-item dropdown">
-								<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							<li className="nav-item dropdown">
+								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 									Dropdown
 								</a>
-								<ul class="dropdown-menu">
-									<li><a class="dropdown-item" href="#">Action</a></li>
-									<li><a class="dropdown-item" href="#">Another action</a></li>
-									<li><hr class="dropdown-divider" /></li>
-									<li><a class="dropdown-item" href="#">Something else here</a></li>
+								<ul className="dropdown-menu">
+									<li><a className="dropdown-item" href="#">Action</a></li>
+									<li><a className="dropdown-item" href="#">Another action</a></li>
+									<li><hr className="dropdown-divider" /></li>
+									<li><a className="dropdown-item" href="#">Something else here</a></li>
 								</ul>
 							</li>
-							<li class="nav-item">
-								<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+							<li className="nav-item">
+								<a className="nav-link disabled" aria-disabled="true">Disabled</a>
 							</li>
-							<li class="nav-item">
+							<li className="nav-item d-none d-md-block">
 								<BtnFavorites />
 							</li>
 						</ul>
